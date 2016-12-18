bc62fb51fd6a4ed194b9d5454896344a15577b73
Zerss <zersssin@gmail.com>
2016-12-18 17:16:59 +0100

    Added Easing elements

--- storygen/Osb.cs
+++ storygen/Osb.cs
@@ -12,24 +12,37 @@ namespace storygen
 
         public Origin TopLeft, TopCentre, TopRight, CentreLeft, Centre, CentreRight, BottomLeft, BottomCentre, BottomRight;
 
+        public Easing Out, In, QuadIn, QuadOut, QuadInOut, CubicIn, CubicOut, CubicInOut, QuartIn,
+                      QuartOut, QuartInOut, QuintIn, QuintOut, QuintInOut, SineIn, SineOut, SineInOut, ExpoIn, ExpoOut,
+                      ExpoInOut, CircIn, CircOut, CircInOut, ElasticIn, ElasticOut, ElasticHalfOut, ElasticQuarterOut, ElasticInOut, BackIn,
+                      BackOut, BackInOut, BounceIn, BounceOut, BounceInOut, Linear;
+
         public Osb()
         {
+            // Setting up Layers
             Background = new Layer(0);
             Fail = new Layer(1);
             Pass = new Layer(2);
             Foreground = new Layer(3);
 
-            TopLeft = new Origin("TopLeft");
-            TopCentre = new Origin("TopCentre");
-            TopRight = new Origin("TopRight");
+            // Setting up Origins
+            TopLeft = new Origin("TopLeft"); TopCentre = new Origin("TopCentre"); TopRight = new Origin("TopRight");
+            CentreLeft = new Origin("CentreLeft"); Centre = new Origin("Centre"); CentreRight = new Origin("CentreRight");
+            BottomLeft = new Origin("BottomLeft"); BottomCentre = new Origin("BottomCentre"); BottomRight = new Origin("BottomRight");
 
-            CentreLeft = new Origin("CentreLeft");
-            Centre = new Origin("Centre");
-            CentreRight = new Origin("CentreRight");
-
-            BottomLeft = new Origin("BottomLeft");
-            BottomCentre = new Origin("BottomCentre");
-            BottomRight = new Origin("BottomRight");
+            // Setting up Easings
+            Linear = new Easing("Linear");
+            Out = new Easing("Out"); In = new Easing("In");
+            QuadIn = new Easing("QuadIn"); QuadOut = new Easing("QuadOut"); QuadInOut = new Easing("QuadInOut");
+            CubicIn = new Easing("CubicIn"); CubicOut = new Easing("CubicOut"); CubicInOut = new Easing("CubicInOut");
+            QuartIn = new Easing("QuartIn"); QuartOut = new Easing("QuartOut"); QuartInOut = new Easing("QuartInOut");
+            QuintIn = new Easing("QuintIn"); QuintOut = new Easing("QuintOut"); QuintInOut = new Easing("QuintInOut");
+            SineIn = new Easing("SineIn"); SineOut = new Easing("SineOut"); SineInOut = new Easing("SineInOut");
+            ExpoIn = new Easing("ExpoIn"); ExpoOut = new Easing("ExpoOut"); ExpoInOut = new Easing("ExpoInOut");
+            CircIn = new Easing("CircIn"); CircOut = new Easing("CircOut"); CircInOut = new Easing("CircInOut");
+            ElasticIn = new Easing("ElasticIn"); ElasticOut = new Easing("ElasticOut"); ElasticHalfOut = new Easing("ElasticHalfOut"); ElasticQuarterOut = new Easing("ElasticQuarterOut"); ElasticInOut = new Easing("ElasticInOut");
+            BackIn = new Easing("BackIn"); BackOut = new Easing("BackOut"); BackInOut = new Easing("BackInOut");
+            BounceIn = new Easing("BounceIn"); BounceOut = new Easing("BounceOut"); BounceInOut = new Easing("BounceInOut");
         }
 
         public void render()
