2d973d8918963dd0f2f9b5e13ed88f2b13a55e30
Zerss <zersssin@gmail.com>
2016-12-18 16:52:04 +0100

    Added initial Easing function

---
+++ storygen/Easing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storygen
+{
+    class Easing
+    {
+        String EasingName;
+        int EasingID;
+        String[] Easings = new string[] { "Out", "In", "QuadIn", "QuadOut", "QuadInOut", "CubicIn", "CubicOut", "CubicInOut", "QuartIn",
+                                          "QuartOut", "QuartInOut", "QuintIn", "QuintOut", "QuintInOut", "SineIn", "SineOut", "SineInOut", "ExpoIn", "ExpoOut",
+                                          "ExpoInOut", "CircIn", "CircOut", "CircInOut", "ElasticIn", "ElasticOut", "ElasticHalfOut", "ElasticQuarterOut", "ElasticInOut", "BackIn",
+                                          "BackOut", "BackInOut", "BounceIn", "BounceOut", "BounceInOut" };
+
+        public Easing(String EasingName)
+        {
+            this.EasingName = EasingName;
+
+            EasingID = 0;
+            for (int i = 1; i <= Easings.Length; i++)
+            {
+                if (Easings[i - 1] == EasingName) EasingID = i;
+            }
+        }
+
+        public String getName()
+        {
+            return EasingName;
+        }
+
+        public int getID()
+        {
+            return EasingID;
+        }
+    }
+}
