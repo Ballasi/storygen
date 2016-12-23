e9fc3fe00d079ff5d89d2552fe2be68033d7d5fe
Zerss <zersssin@gmail.com>
2016-12-23 14:54:04 +0100

    Added Color.cs

---
+++ storygen/other/Color.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storygen.Other
+{
+    class Color
+    {
+        public double R;
+        public double G;
+        public double B;
+
+        public Color()
+        {
+        }
+
+        public Color(double R, double G, double B)
+        {
+            this.R = R;
+            this.G = G;
+            this.B = B;
+        }
+
+    }
+}
